bade89cae05375a0caf356443ec3bfaaf17fc134
straycatse <developer@straycat.nu>
2024-04-19 16:21:25 +0200

    Adding rewrite mode

--- SchemaInjector.tsx
+++ SchemaInjector.tsx
@@ -4,14 +4,26 @@ import Script from "next/script";
 interface SchemaInjectorProps {
   schema: Schema;
   mode?: "default" | "rewriteUrl";
+  fromDomain?: string;
+  toDomain?: string;
   scriptStrategy?: "beforeInteractive" | "afterInteractive";
 }
 
 export default function SchemaInjector({
   schema,
   mode = "default",
+  fromDomain = "https://www.example.com",
+  toDomain = "https://example.se",
   scriptStrategy = "beforeInteractive",
 }: SchemaInjectorProps) {
+  if (mode === "rewriteUrl") {
+    schema["@graph"].forEach((graph) => {
+      if (graph.url) {
+        graph.url = graph.url.replace(fromDomain, toDomain);
+      }
+    });
+  }
+
   return (
     <Script
       id="json-ld-schema"
