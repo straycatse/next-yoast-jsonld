678252209f3e6835b8da60a088cc014ca7998db5
straycatse <developer@straycat.nu>
2024-04-22 13:39:27 +0200

    Trying some modifications

--- SchemaInjector.tsx
+++ SchemaInjector.tsx
@@ -12,16 +12,43 @@ interface SchemaInjectorProps {
 export default function SchemaInjector({
   schema,
   mode = "default",
-  fromDomain = "https://www.example.com",
-  toDomain = "https://example.se",
+  fromDomain,
+  toDomain,
   scriptStrategy = "beforeInteractive",
 }: SchemaInjectorProps) {
-  if (mode === "rewriteUrl") {
-    schema["@graph"].forEach((graph) => {
-      if (graph.url) {
-        graph.url = graph.url.replace(fromDomain, toDomain);
+  if (mode === "rewriteUrl" && fromDomain && toDomain) {
+    const searchRegexp = new RegExp(fromDomain, "g");
+    const rewrittenSchema = JSON.parse(
+      JSON.stringify(schema).replace(searchRegexp, toDomain)
+    );
+
+    console.log("Rewritten schema", rewrittenSchema);
+
+    return (
+      <Script
+        id="json-ld-schema"
+        type="application/ld+json"
+        strategy={scriptStrategy}
+        dangerouslySetInnerHTML={{
+          __html: JSON.stringify(rewrittenSchema),
+        }}
+      />
+    );
+    /*  schema["@graph"].forEach((graph) => {
+      if (graph["@type"] === "ImageObject") {
+        return;
       }
-    });
+      if (graph.url) {
+        const newUrl = graph.url.replace(fromDomain, toDomain);
+        console.log("Replacing URL", graph.url, "with", newUrl);
+        graph.url = newUrl;
+      }
+      if (graph["@id"]) {
+        const newId = graph["@id"].replace(fromDomain, toDomain);
+        console.log("Replacing @id", graph["@id"], "with", newId);
+        graph["@id"] = newId;
+      }
+    }); */
   }
 
   return (
